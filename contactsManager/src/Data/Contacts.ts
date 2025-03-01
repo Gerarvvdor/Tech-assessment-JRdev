e59ed1b423dafa0e33cf5cdc3f490e83a6c43a0c
Gerarvvdor <geraraov@gmail.com>
2025-02-28 19:43:40 -0600

    Fixed: read and delete 'crud' fixed

---
+++ contactsManager/src/Data/Contacts.ts
@@ -0,0 +1,3 @@
+import type { DialogData } from "../Types/types";
+
+export const Contacts: DialogData[] = []
--- contactsManager/src/app/app.routes.ts
+++ contactsManager/src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-  {
-    path: 'contact form',
-    component: ContactFormComponent
-  },{
-    path: 'root',
-    component: AppComponent
-  }
-];
+export const routes: Routes = [];
